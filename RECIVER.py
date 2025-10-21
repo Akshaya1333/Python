9c31b4f1b7b983901d7d1f7eb0660d053eb9092c
Akshaya1333 <147792216+Akshaya1333@users.noreply.github.com>
2025-10-21 19:33:56 +0530

    Create RECIVER.py

---
+++ RECIVER.py
@@ -0,0 +1,13 @@
+import socket
+s=socket.socket()
+s.connect(("localhost",8020))
+while (1):
+    msg=s.recv(1).decode()
+    print("Received msg --> ",msg)
+    x=int(msg)
+    if x==0:
+        x+=1
+        s.send(str(x).encode())
+    else:
+        x-=1
+        s.send(str(x).encode())
